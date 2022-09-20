89775b32119061a04bc1b7b269f88676aa913884
Paulo092 <psolsilva@gmail.com>
2022-09-20 17:16:18 -0300

    :sparkles: feat: Geração da árvore sintática

--- header.h
+++ header.h
@@ -2,13 +2,18 @@
 
 //header.h
 
-enum noh_type {PROGRAM, ASSIGN, SUM, MINUS, MULTI, DIVIDE, PRINT, POW};
+enum noh_type {PROGRAM, ASSIGN, SUM, MINUS, MULTI, DIVIDE, PRINT, POW, GENERIC, STMT, FLOAT, IDENT, INTEGER, PAREN};
 
 struct noh {
 	int id, childcount;
 	enum noh_type type;
-	
+
+	double value;
+	char *name;
+
 	struct noh *children[1];
 };
 
 typedef struct noh noh;
+
+noh *create_noh(enum noh_type nt, int children);
